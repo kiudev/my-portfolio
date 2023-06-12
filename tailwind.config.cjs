fba1a96e078b5887075a950fd8762be8333ae68b
kiudev <dani.97.saavedra@gmail.com>
2023-06-12 22:33:49 +0200

    add readme

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'night': '#161616',
+			'night-300': '#404040',
+			'light': '#E8E8E8',
+			'blue-300': '#50DDE7',
+			'blue-400': '#1EBCDC',
+			'black': '#000000'
+		},
+	},
+	plugins: [],
+}
